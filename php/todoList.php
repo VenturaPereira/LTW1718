6ada9bf6911c95618798e3a06b386725fcfeb120
VenturaPereira <venturasousapereira@hotmail.com>
2017-11-28 10:27:16 +0000

    changes to php, created sql

---
+++ php/todoList.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$dbh = new PDO('sqlite:test.db');
+$dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+
+
+
+
+
+
+
+ ?>
