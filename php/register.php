761e30f11db3c4c33d73cdd3904f5afea068b548
VenturaPereira <venturasousapereira@hotmail.com>
2017-11-28 17:28:16 +0000

    changes and error catchin

--- php/register.php
+++ php/register.php
@@ -5,7 +5,7 @@ $password= $_POST[""];
 $email = $_POST[""];
 $confirmPassword= $_POST[""];
 //abrir db
-$dbh = new PDO('sqlite:test.db');
+$dbh = new PDO('sqlite:taskify.db');
 $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
@@ -14,6 +14,7 @@ $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
 if($confirmPassword == $password){
+  try{
 //insert statements
 $sqlToInsert='INSERT INTO user (id,name,password,email) VALUES(:id, :name, :password,:email)';
 $stmt=$dbh->prepare($sqlToInsert);
@@ -24,6 +25,9 @@ $stmt->bindParam(':password',$password);
 $stmt->bindParam(':email',$email);
 // execute statement
 $stmt->execute();
+} catch (PDOException $e){
+  echo $e->getMessage();
+}
 
 }else {
   echo "Passwords don't match!";
--- php/todoList.php
+++ php/todoList.php
@@ -1,15 +1,9 @@
 <?php
 
 
-$dbh = new PDO('sqlite:test.db');
+$dbh = new PDO('sqlite:taskify.db');
 $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-
-
-
-
 
 
 
