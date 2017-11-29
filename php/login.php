ce45766cebf67f06e6d1658296ea2ac5240ae1c5
VenturaPereira <venturasousapereira@hotmail.com>
2017-11-29 12:05:47 +0000

    stuff

---
+++ php/login.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$dbh = new PDO('sqlite:taskify.db');
+$dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+function isLoginCorrect($username, $password) {
+   $stmt = $dbh->prepare('SELECT * FROM user WHERE usr_username = ? AND usr_password = ?');
+   $stmt->execute(array($username, sha1($password)));
+   return $stmt->fetch() !== false;
+ }
+ ?>
